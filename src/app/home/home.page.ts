01c3dd42b0c6c20f91656bf5445446deee8e2215
Bruno Eduardo <brunodudu@github.com>
2019-05-23 16:08:50 -0300

    Logout da home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  private accessToken: string;
+
+  constructor(private storage:Storage,
+    private router:Router) {}
+
+  ngOnInit() {
+    this.storage.get('BuJoToken').then(
+      (val) => {
+        this.accessToken = val;
+        if (this.accessToken == '') {
+          this.router.navigate(['login']);
+        }
+      }
+    ).catch(
+      () => {
+        this.router.navigate(['login']);
+      }
+    );
+  }
+
+  logout() {
+    this.storage.set('BuJoToken','');
+    this.router.navigate(['login']);
+  }
 
 }
